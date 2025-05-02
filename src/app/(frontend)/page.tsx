9da88e436f660de88c9899c75182642aa0dbd304
Fahim-Arefin <fahimarefin57@gmail.com>
2025-05-02 11:23:49 +0600

    root index page is cached out

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = 'force-dynamic'
+
 import { notFound } from 'next/navigation'
 import { queryPageBySlug } from './[slug]/page'
 import { renderPageContent } from '@/components/PageContent'
